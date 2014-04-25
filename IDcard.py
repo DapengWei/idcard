ef37a5145e6407fed3fb618f0e7b7db26ed69cb0
DapengWei <wdp137@gmail.com>
2014-04-26 01:03:06 +0800

    fix some encode error and add exceptional handling

--- IDcard.py
+++ IDcard.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 import geofind
+
 #function
 
 #convert string to int list
 def convID(list_num):
 	int_num = []
-	for i in range(17):
-		int_num.append(int(list_num[i]))
+	for i in xrange(17):
+		try:
+			int_num.append(int(list_num[i]))
+		except ValueError:
+			return False
 	if list_num[17] == "x" or list_num[17] == "X":
 		int_num.append(10)
 	else:
@@ -18,7 +25,7 @@ def verifyID(int_verify):
 	key=(7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2)
 	key2=(1,0,10,9,8,7,6,5,4,3,2)
 	all= 0
-	for i in range(17):
+	for i in xrange(17):
 		sum = int_verify[i]*key[i]
 		all = all + sum	
 	verify_num = key2[all%11]
@@ -32,41 +39,46 @@ def birthdayID(ID_birthday):
 	year = int(ID_birthday[0])*1000+int(ID_birthday[1])*100+int(ID_birthday[2])*10+int(ID_birthday[3])
 	month = int(ID_birthday[4])*10+int(ID_birthday[5])
 	day = int(ID_birthday[6])*10+int(ID_birthday[7])
-	birthday_word = str(year)+"年"+str(month)+"月"+str(day)+"日"
+	birthday_word = str(year)+u"年"+str(month)+u"月"+str(day)+u"日"
 	return birthday_word
+
 #begin
 while 1:
-	print "请输入18位身份证号码:",
+	print u"请输入18位身份证号码:",
 	ID_list = raw_input()	
 	
 	if len(ID_list) != 18:
-		print"输入错误，位数不对，重新输入!"
+		print u"输入错误，位数不对，重新输入!"
 		continue
 	else:
-		ID_num = convID(ID_list)
-		if verifyID(ID_num):
-			break
+		if not convID(ID_list):
+			print u"输入错误，这不是合法的身份证号码，重新输入!"
+			continue
 		else:
-			print"输入错误，这不是合法的身份证号码，请重新输入!"
-			continue		
+			ID_num = convID(ID_list)
+			if verifyID(ID_num):
+				break
+			else:
+				print u"输入错误，这不是合法的身份证号码，请重新输入!"
+				continue		
 	
 
 
 
 
 address_num = []
-for i in range(6):
+for i in xrange(6):
 	address_num.append(ID_num[i])
 address = geofind.geoloc(address_num)
 
 birthday_num = []
-for i in range(6,14,1):
+for i in xrange(6,14,1):
 	birthday_num.append(ID_num[i])	
 birthday = birthdayID(birthday_num)
 
 if int(ID_num[16])%2 == 0 :
-	sex = "女"
+	sex = u"女"
 else:
-	sex = "男"
+	sex = u"男"
 
-print "该公民 性别:",sex,"籍贯:",address,"生日:",birthday
+print u"该公民 性别:",sex,u"籍贯:",address,u"生日:",birthday
--- geofind.py
+++ geofind.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
 def geoloc(ID_geonum):
-	geofile = file("geo.txt","r")
-	geoline = geofile.readlines()
-	for i in range(len(geoline)):
+	try:
+		with file("geo.txt","r") as geofile:
+			geoline = geofile.readlines()
+	except IOError:
+		print u"文件读取错误!请检查地理信息文件geo.txt是否存在并在同一级目录"
+		return u"文件错误！"
+	for i in xrange(len(geoline)):
 		geoline[i]=geoline[i].strip("\n")
 	address_dict = {}
-	for i in range(0,len(geoline),2):
+	for i in xrange(0,len(geoline),2):
 		address_dict[int(geoline[i])] = geoline[i+1]
+	
 	address_num_a = 0
 	address_num_b = 0
 	address_num_c = 0
-	for i in range(2):
+	for i in xrange(2):
 		address_num_a=address_num_a+int(ID_geonum[i])*(10**(5-i))
-	for i in range(4):
+	for i in xrange(4):
 		address_num_b=address_num_b+int(ID_geonum[i])*(10**(5-i))
-	for i in range(6):
+	for i in xrange(6):
 		address_num_c=address_num_c+int(ID_geonum[i])*(10**(5-i))
 	address_word_a = address_dict[address_num_a]
 	address_word_b = address_dict[address_num_b]
 	address_word_c = address_dict[address_num_c]
-	address_word = address_word_a + address_word_b  +address_word_c
-	return address_word  
+	address_word = address_word_a + address_word_b  + address_word_c
+	return address_word.decode()
